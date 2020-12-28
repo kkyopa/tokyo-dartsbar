fb6efcf280cb65cc709c46f0e36420b8e3c7b5a4
川田京助 <kkyopa@gmail.com>
2020-12-29 00:39:40 +0900

    コンポーネント記載

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import GenericTemplate from "../templates/GenericTemplate";
+import SimpleMenu from "../atoms/pul";
+import ContainedButtons from "../atoms/buttom";
+import SimplePaper from "../atoms/Paper";
 
 const HomePage: React.FC = () => {
   return (
     <GenericTemplate title="Tokyo_DartsBar">
+      <img src="/images/title.png" />
       <>検索してみよう</>
+      <SimpleMenu />
+      <ContainedButtons />
+      <SimplePaper/>
     </GenericTemplate>
   );
 };
