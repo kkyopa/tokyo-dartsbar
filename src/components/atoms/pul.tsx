f439219439850237923ee044ea8da247ac043a3e
川田京助 <kkyopa@gmail.com>
2020-12-29 20:50:14 +0900

    state

--- src/components/atoms/pul.tsx
+++ src/components/atoms/pul.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import ContainedButtons from "../atoms/buttom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,8 +25,29 @@ interface TodoProps {
 
 const cours = Array();
 const tokyo = [
-  '足立区', '荒川区', '板橋区', '江戸川区', '大田区', '葛飾区', '北区', '江東区', '品川区', '渋谷区', '新宿区', '杉並区',
-  '墨田区', '世田谷', '台東区', '中央区', '千代田', '豊島区', '中野区', '練馬区', '文京区', '港区', '目黒区'
+  "足立区",
+  "荒川区",
+  "板橋区",
+  "江戸川区",
+  "大田区",
+  "葛飾区",
+  "北区",
+  "江東区",
+  "品川区",
+  "渋谷区",
+  "新宿区",
+  "杉並区",
+  "墨田区",
+  "世田谷",
+  "台東区",
+  "中央区",
+  "千代田",
+  "豊島区",
+  "中野区",
+  "練馬区",
+  "文京区",
+  "港区",
+  "目黒区",
 ];
 
 for (var i = 0; i < tokyo.length; i++) {
@@ -38,6 +60,11 @@ export default function SimpleSelect(props) {
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setAge(event.target.value as string);
+  };
+
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(() => count + 1);
   };
 
   return (
@@ -56,6 +83,8 @@ export default function SimpleSelect(props) {
           </MenuItem>
           {cours}
         </Select>
+        <p>You clicked {count} times</p>
+        <button onClick={handleClick}>+1</button>
         <ul>
           {props.items.map((list) => (
             <li>{list.text}</li>
