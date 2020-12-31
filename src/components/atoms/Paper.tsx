713e932811241bcc40655bd494c328c672bb2a2a
川田京助 <kkyopa@gmail.com>
2020-12-31 18:44:12 +0900

    不要なコンポーネントの削除

--- src/components/atoms/Paper.tsx
+++ src/components/atoms/Paper.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Bar from "../atoms/Bar";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      '& > *': {
+      display: "flex",
+      flexWrap: "wrap",
+      "& > *": {
         margin: theme.spacing(1),
         width: theme.spacing(16),
         height: theme.spacing(16),
       },
     },
-  }),
+  })
 );
 
 export default function SimplePaper() {
@@ -21,20 +22,7 @@ export default function SimplePaper() {
 
   return (
     <div className={classes.root}>
-      <Paper>
-      スリーコード
-      </Paper>
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
-      <Paper />
+      <Paper>スリーコード</Paper>
     </div>
   );
 }
